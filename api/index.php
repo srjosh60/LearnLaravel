c4f0490361b8afda81a8e33dac796a1b2ddcf4e3
srjosh60 <srjosh60@gmail.com>
2026-06-19 18:30:59 +0700

    Simplify Vercel entry point

--- api/index.php
+++ api/index.php
@@ -1,5 +1,7 @@
 <?php
 
+define('LARAVEL_START', microtime(true));
+
 $root = __DIR__ . '/..';
 
 // Create required storage directories in /tmp (Vercel read-only filesystem)
@@ -17,30 +19,11 @@ foreach ($dirs as $dir) {
     }
 }
 
-// Copy view files to /tmp if not already there
-$viewCacheSrc = $root . '/storage/framework/views';
-if (is_dir($viewCacheSrc)) {
-    foreach (glob($viewCacheSrc . '/*.php') as $file) {
-        $dest = '/tmp/storage/framework/views/' . basename($file);
-        if (!file_exists($dest)) {
-            copy($file, $dest);
-        }
-    }
-}
-
 require_once $root . '/vendor/autoload.php';
 
 $app = require_once $root . '/bootstrap/app.php';
 
 $app->useStoragePath('/tmp/storage');
-$app->bootstrapWith([
-    \Illuminate\Foundation\Bootstrap\LoadEnvironmentVariables::class,
-    \Illuminate\Foundation\Bootstrap\LoadConfiguration::class,
-    \Illuminate\Foundation\Bootstrap\HandleExceptions::class,
-    \Illuminate\Foundation\Bootstrap\RegisterFacades::class,
-    \Illuminate\Foundation\Bootstrap\RegisterProviders::class,
-    \Illuminate\Foundation\Bootstrap\BootProviders::class,
-]);
 
 $kernel = $app->make(\Illuminate\Contracts\Http\Kernel::class);
 
